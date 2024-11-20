d26cbec72db259d89ff9dba1f1c495d3086afb15
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-11-20 21:30:43 +0100

    added multiplication by qft

--- sheet6/sheet6.r
+++ sheet6/sheet6.r
@@ -2,7 +2,7 @@ library('qsimulatR')
 
 #addition by fourier transform
 
-n <- 3 
+n <- 8 
 
 x = 2
 
@@ -24,8 +24,33 @@ for (j in 1:n) {
 
     theta = 2 * pi *  x / (2^(n-j+1))
     q <- sqgate(bit=j, M=array(as.complex(c(1,0,0,exp(1i*theta) )), dim=c(2,2))) * q
-
 }
 
 q <- qft(q, inverse=TRUE)
 print(q)
+
+
+# multiply and add 
+a <- 3
+x <- 2
+
+coefs <- rep(0, 2^n)
+coefs[4] <- 1
+
+q <- qstate(n, coefs=coefs, basis=as.character(0:(2^n-1)))
+
+print(q)
+print('')
+q <- qft(q)
+
+for (j in 1:n) {
+
+    theta = 2 * pi *  x / (2^(n-j+1))
+    q <- sqgate(bit=j, M=array(as.complex(c(1,0,0,exp(1i*theta*a) )), dim=c(2,2))) * q
+}
+
+q <- qft(q, inverse=TRUE)
+print(q)
+
+
+
