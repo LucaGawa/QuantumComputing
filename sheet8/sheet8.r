1ff89c2b86ea1c6d8e2c4d386803e4a5cd2a6f32
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-11-30 23:52:20 +0100

    implemented harmonic potential, but not sure, if it works

--- sheet8/sheet8.r
+++ sheet8/sheet8.r
@@ -3,6 +3,11 @@ library('qsimulatR')
 n <- 6
 psi <- qstate(n)
 psi <- X(n) * psi # apply a delta peak in position space
+# for (i in 1:(n)) {
+#   psi <- X(i) * psi
+#   psi <- H(i) * psi
+# }
+# psi <- H(2) * psi
 # mes = measure(psi) # measure the state
 # plot(hist(mes)) # plot the histogram of the state
 
@@ -10,60 +15,71 @@ Rtheta <- function(i, theta) {
   return (sqgate(bit=i, M=array(as.complex(c(1,0,0,exp(1i*theta))), dim=c(2,2))))
 }
 
-# psi <- SWAP(c(1,n)) * psi
-# psi <- SWAP(c(2,n-1)) * psi
-# psi <- SWAP(c(3,n-2)) * psi
 
-time.evolution <- function(psi, dt) {
-# dt <- 0.1
+free.time.evolution <- function(psi, dt) {
 n <- psi@nbits
 dp <- 2*pi/(2^n)
 a <- pi^2/(n^2)
 b <- pi*dp/n
 c <- dp^2
-# a <- pi^2
-# b <- pi^2/2^(n-1)
-# c <- (pi^2/2^n)^n
 
 q <- qft(psi)
 
+# apply the global phase
 theta <- -n^2*a*dt/2
-# theta <- -dt/2*a
-#
 q <-  Rtheta(1,theta)*(X(1)*(Rtheta(1, theta) * (X(1) * q)))
-# q <- X(1) * q
-# q <- Rtheta(1, theta) * q
-# q <- X(1) * q
-# q <- Rtheta(1, theta) * q
 
 for (j in 1:n) {
-  # theta <- (2*b*2**(n-j-1)-c*2**(2*n-2-2*j))*dt/2
-  # theta <- n*dt*b*2^(j-1)-dt/2*c*4^(j-1)
-  theta <- 2*n*b*dt/2*2^(j-1)-c*dt/2*2^(2*j-2)
+  theta <- dt/2*(n*b*2^(j)-c*2^(2*j-2))
 
   q <- Rtheta(j, theta) * q
   for (l in 1:n){
     if (l != j) {
-      # theta <- -dt*c*2^(l+j-2)
-      # theta <- -c*2**(2*n-2-l-j)*dt/2
       theta <- -c*dt/2*2^(l+j-2)
       q <- cqgate(bits=c(l,j), gate=Rtheta(j, theta)) * q
     }
   }
 }
-
-
 q <- qft(q, inverse=TRUE)
+return(q)
+}
+
+harmonic.potential <- function(psi, dt, gamma, L){
+  n <- psi@nbits
+  dx <- L/(2^n)
+  a <- gamma*(L/2)^2/(n^2)
+  b <- gamma*L/2*dx/n
+  c <- gamma*dx^2
+
+# apply the global phase
+theta <- -n^2*a*dt/2
+q <-  Rtheta(1,theta)*(X(1)*(Rtheta(1, theta) * (X(1) * psi)))
+
+for (j in 1:n) {
+  theta <- dt/2*(n*b*2^(j)-c*2^(2*j-2))
+
+  q <- Rtheta(j, theta) * q
+  for (l in 1:n){
+    if (l != j) {
+      theta <- -c*dt/2*2^(l+j-2)
+      q <- cqgate(bits=c(l,j), gate=Rtheta(j, theta)) * q
+    }
+  }
+}
   return(q)
 }
 
 # mes <- measure(psi, rep=1000)
 # plot(hist(mes,only.nonzero=FALSE, freq=FALSE))
 
-for (i in (1:100)) {
-  psi <- time.evolution(psi, 0.1)
-mes <- measure(psi, rep=1000)
-plot(hist(mes,only.nonzero=FALSE, freq=FALSE))
+dt <- 1
+for (i in (1:10)) {
+  psi <- harmonic.potential(psi, dt, 1, 1000)
+  psi <- free.time.evolution(psi, dt)
+  if (i %% 1 == 0) {
+  mes <- measure(psi, rep=100000)
+  plot(hist(mes,only.nonzero=FALSE, freq=FALSE))
+  }
 }
 
 
